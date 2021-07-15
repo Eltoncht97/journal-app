f2008461a0764cc17d7a9a48170f847490b8735a
Elton Chavez <elton.mct@gmail.com>
2021-07-15 13:41:30 -0300

    animations added

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -14,7 +14,7 @@ export const JournalEntry = ({ id, title, body, date, url }) => {
 
   return (
     <div
-      className="journal__entry"
+      className="journal__entry animate__animated animate__fadeIn animate__faster"
       onClick={handleEntryClick}
     >
 
